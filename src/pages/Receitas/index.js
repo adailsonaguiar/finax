e8c5232ad5ebc0b2a91ace473fca896c276fa743
Adailson Aguiar <adailsonacj@live.com>
2019-10-18 20:59:32 -0300

    tela de receitas

---
+++ src/pages/Receitas/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Header from '../../components/Header';
+
+const Receitas = () => {
+  return (
+    <View>
+      <Header title="Receitas" />
+      <Text>Tela Receitas</Text>
+    </View>
+  );
+};
+
+export default Receitas;
