5787299773e92af05f01578a933d1c79914a2de9
Adailson Aguiar <adailsonacj@live.com>
2019-10-21 23:38:28 -0300

    alterar layout das linhas (despesas)

--- src/pages/Despesas/index.js
+++ src/pages/Despesas/index.js
@@ -24,6 +24,36 @@ const Despesas = () => {
       <View style={styles.card}>
         <ScrollView>
           <View style={styles.rowDespesa}>
+            <View style={styles.row}>
+              <Text style={styles.txtIntro}>Churrascaria flor do nordeste</Text>
+              <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
+            </View>
+            <View style={styles.row}>
+              <View style={styles.observacao}>
+                <Text style={{ fontSize: 10 }}>Alimentação</Text>
+              </View>
+              <View style={styles.observacao}>
+                <Text style={{ fontSize: 10 }}>30/10/2019</Text>
+              </View>
+              <Text style={{ fontSize: 10 }}>Pago</Text>
+            </View>
+          </View>
+          <View style={styles.rowDespesa}>
+            <View style={styles.row}>
+              <Text style={styles.txtIntro}>Churrascaria flor do nordeste</Text>
+              <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
+            </View>
+            <View style={styles.row}>
+              <View style={styles.observacao}>
+                <Text style={{ fontSize: 10 }}>Alimentação llkas</Text>
+              </View>
+              <View style={styles.observacao}>
+                <Text style={{ fontSize: 10 }}>30/10/2019</Text>
+              </View>
+              <Text style={{ fontSize: 10 }}>Pago</Text>
+            </View>
+          </View>
+          {/* <View style={styles.rowDespesa}>
             <View style={styles.col}>
               <Text style={styles.txtIntro}>Churrascaria flor do nordeste</Text>
               <Text style={{ fontSize: 10 }}>Alimentação</Text>
@@ -33,74 +63,26 @@ const Despesas = () => {
               <Text style={{ fontSize: 10 }}>Pago</Text>
             </View>
           </View>
-          <View style={styles.row}>
-            <Text style={styles.txtIntro}>Despesas</Text>
-            <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
+          <View style={styles.rowDespesa}>
+            <View style={styles.col}>
+              <Text style={styles.txtIntro}>Churrascaria flor do nordeste</Text>
+              <Text style={{ fontSize: 10 }}>Alimentação</Text>
+            </View>
+            <View style={styles.col}>
+              <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
+              <Text style={{ fontSize: 10 }}>Pago</Text>
+            </View>
           </View>
-          <View style={styles.row}>
-            <Text style={styles.txtIntro}>Despesas</Text>
-            <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.txtIntro}>Despesas</Text>
-            <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.txtIntro}>Despesas</Text>
-            <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.txtIntro}>Despesas</Text>
-            <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.txtIntro}>Despesas</Text>
-            <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.txtIntro}>Despesas</Text>
-            <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.txtIntro}>Despesas</Text>
-            <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.txtIntro}>Despesas</Text>
-            <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.txtIntro}>Despesas</Text>
-            <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.txtIntro}>Despesas</Text>
-            <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.txtIntro}>Despesas</Text>
-            <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.txtIntro}>Despesas</Text>
-            <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.txtIntro}>Despesas</Text>
-            <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.txtIntro}>Despesas</Text>
-            <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.txtIntro}>Despesas</Text>
-            <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
-          </View>
-          <View style={styles.row}>
-            <Text style={styles.txtIntro}>Despesas</Text>
-            <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
-          </View>
+          <View style={styles.rowDespesa}>
+            <View style={styles.col}>
+              <Text style={styles.txtIntro}>Churrascaria flor do nordeste</Text>
+              <Text style={{ fontSize: 10 }}>Alimentação</Text>
+            </View>
+            <View style={styles.col}>
+              <Text style={{ color: '#f39c12ff' }}>R$ 342,32</Text>
+              <Text style={{ fontSize: 10 }}>Pago</Text>
+            </View>
+          </View> */}
         </ScrollView>
       </View>
     </View>
@@ -128,8 +110,9 @@ const styles = StyleSheet.create({
   },
   row: {
     flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginBottom: 10
+    justifyContent: 'space-between',
+    marginBottom: 10,
+    borderBottomColor: '#5c5151'
   },
   vlrPago: {
     fontSize: 14,
@@ -147,12 +130,15 @@ const styles = StyleSheet.create({
     height: 265,
     margin: 10,
     borderRadius: 14,
-    padding: 7,
+    padding: 10,
     flexDirection: 'column'
   },
   rowDespesa: {
-    flexDirection: 'row',
+    flexDirection: 'column',
     justifyContent: 'space-between',
-    marginBottom: 10,
+    marginBottom: 10
+  },
+  observacao: {
+    width: 60
   }
 });
