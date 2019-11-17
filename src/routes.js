28ea4c0ffb9b6bc51eedaca8d9d99976ba43879e
Adailson Aguiar <adailsonacj@live.com>
2019-11-17 08:42:05 -0300

    atualização da navegação no app

--- src/routes.js
+++ src/routes.js
@@ -3,68 +3,69 @@ import {
   createAppContainer,
   createStackNavigator,
   createBottomTabNavigator,
-  createSwitchNavigator
+  createSwitchNavigator,
+  createDrawerNavigator
 } from 'react-navigation';
 
 import Home from './pages/Home';
 import Despesas from './pages/Despesas';
 import Carteiras from './pages/Carteiras';
+import Header from './components/Header';
 
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import NovaDespesa from './pages/NovaDespesa/NovaDespesa';
 
-const DespesasTab = createSwitchNavigator({
-  Despesas: {
-    screen: Despesas
-  },
-  NovaDespesa: {
-    screen: NovaDespesa
-  }
-});
-
-export default createAppContainer(
-  createBottomTabNavigator(
-    {
-      Home: {
-        screen: Home,
-        navigationOptions: () => ({
-          tabBarIcon: ({ tintColor }) => (
-            <Icon name='home' color={tintColor} size={22} />
-          )
-        })
-      },
-      Despesas: {
-        screen: DespesasTab,
-        navigationOptions: {
-          tabBarIcon: ({ tintColor }) => (
-            <Icon name='money-bill' color={tintColor} size={22} />
-          )
-        }
-      },
-      Carteiras: {
-        screen: Carteiras,
-        navigationOptions: {
-          tabBarIcon: ({ tintColor }) => (
-            <Icon name='wallet' color={tintColor} size={22} />
-          )
-        }
+const BottomTab = createBottomTabNavigator(
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: () => ({
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name='home' color={tintColor} size={22} />
+        )
+      })
+    },
+    Despesas: {
+      screen: Despesas,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name='money-bill' color={tintColor} size={22} />
+        )
       }
     },
-    {
-      defaultNavigationOptions: {
-        headerTintColor: '#000',
-        headerTitle: 'Agosto',
-        tabBarOptions: {
-          activeTintColor: '#f39c12ff',
-          showLabel: false
-        }
-      },
-      mode: 'modal'
+    Carteiras: {
+      screen: Carteiras,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name='wallet' color={tintColor} size={22} />
+        )
+      }
     }
-  ),
-  createStackNavigator({
+  },
+  {
+    defaultNavigationOptions: {
+      headerTintColor: '#000',
+      headerTitle: 'Agosto',
+      tabBarOptions: {
+        activeTintColor: '#f39c12ff',
+        showLabel: false
+      }
+    },
+    mode: 'modal'
+  }
+);
+
+export default createAppContainer(
+  createDrawerNavigator({
+    //  bottomTabNav
+    BottomTab: {
+      screen: BottomTab
+    },
     NovaDespesa: {
       screen: NovaDespesa
+    },
+    Header: {
+      screen: Header
     }
   })
 );
