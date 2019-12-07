637ca94feb102d44661bcab7ad01d6e12f0d8664
Adailson Aguiar <adailsonacj@live.com>
2019-12-07 16:26:47 -0300

    feat(conta-form): estilezação do form

--- src/pages/ContaForm/ContaForm.js
+++ src/pages/ContaForm/ContaForm.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StatusBar} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
+import DatePicker from 'react-native-datepicker';
+import {TextInputMask} from 'react-native-masked-text';
 
 import {
   Container,
@@ -8,9 +11,18 @@ import {
   BtnFechar,
   TxtBtnFechar,
   Form,
+  InputContainer,
+  Input,
+  Picker,
+  BtnNovaConta,
+  TxtNovaConta,
+  styles,
 } from './styles';
 
 export default function ContaForm({navigation}) {
+  const [date, setDate] = useState(0);
+  const [category, setCategory] = useState(0);
+  const [value, setValue] = useState(0);
   return (
     <Container>
       <StatusBar barStyle="light-content" backgroundColor="#1e1e1e" />
@@ -20,7 +32,54 @@ export default function ContaForm({navigation}) {
           <TxtBtnFechar>X</TxtBtnFechar>
         </BtnFechar>
       </HeaderForm>
-      <Form></Form>
+      <Form>
+        <InputContainer>
+          <Picker
+            selectedValue={category}
+            onValueChange={selected => setCategory(selected)}
+            style={styles.input}>
+            <Picker.Item label="Selecione uma conta" value="" />
+            <Picker.Item label="Conta BB" value="Veículo" />
+            <Picker.Item label="Conta Caixa" value="Farmácia" />
+            <Picker.Item label="Rico" value="Contas Fixas" />
+          </Picker>
+        </InputContainer>
+        <Input placeholder="Nome" />
+        <InputContainer>
+          <Picker
+            selectedValue={category}
+            onValueChange={selected => setCategory(selected)}
+            style={styles.input}>
+            <Picker.Item label="Selecione uma categoria" value="" />
+            <Picker.Item label="Conta Corrente" value="Alimentação" />
+            <Picker.Item label="Conta Poupança" value="Veículo" />
+            <Picker.Item label="Investimento" value="Farmácia" />
+            <Picker.Item label="Carteira" value="Contas Fixas" />
+          </Picker>
+        </InputContainer>
+        <InputContainer>
+          <TextInputMask
+            type={'money'}
+            options={{
+              precision: 2,
+              separator: ',',
+              delimiter: '.',
+              unit: 'R$',
+              suffixUnit: '',
+            }}
+            value={value}
+            onChangeText={value => {
+              setValue(value);
+            }}
+            style={styles.input}
+          />
+        </InputContainer>
+      </Form>
+      <BtnNovaConta
+        activeOpacity={0.9}
+        onPress={() => navigation.navigate('ContaForm')}>
+        <TxtNovaConta>SALVAR</TxtNovaConta>
+      </BtnNovaConta>
     </Container>
   );
 }
--- src/pages/ContaForm/styles.js
+++ src/pages/ContaForm/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import {StyleSheet} from 'react-native';
 
 export const Container = styled.View`
   flex: 1;
@@ -27,5 +28,61 @@ export const TxtBtnFechar = styled.Text`
   font-family: Roboto-Light;
 `;
 export const Form = styled.View`
+  justify-content: center;
   flex: 10;
 `;
+export const InputContainer = styled.View`
+  justify-content: center;
+  margin-bottom: 20px;
+  border-bottom-width: 1px;
+  border-bottom-color: #383642;
+  height: 45px;
+  margin-left: 30px;
+  margin-right: 30px;
+`;
+export const ButtonContainer = styled.View`
+  justify-content: center;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  border-top-width: 1px;
+  border-top-color: #f2f2f2;
+  background: #fff;
+`;
+export const LabelButton = styled.Text`
+  color: #f39c12ff;
+  font-size: 14px;
+  font-weight: bold;
+`;
+export const Picker = styled.Picker`
+  margin-bottom: 20px;
+  color: #95a5a6;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#95a5a6',
+})`
+  margin-bottom: 20px;
+  border-bottom-width: 1px;
+  border-bottom-color: #383642;
+  height: 45px;
+  margin-left: 30px;
+  margin-right: 30px;
+  color: white;
+  font-size: 17px;
+`;
+export const BtnNovaConta = styled.TouchableOpacity`
+  background: #03dac5;
+  height: 61px;
+  align-items: center;
+  justify-content: center;
+`;
+export const TxtNovaConta = styled.Text`
+  color: #e1e1e1;
+  font-family: Roboto-Bold;
+  font-size: 18px;
+`;
+
+export const styles = StyleSheet.create({
+  input: {color: 'white', fontSize: 17},
+});
