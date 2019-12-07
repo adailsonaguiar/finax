9bb33ed98acf5938bcb8c768bb439221fe4cde48
Adailson Aguiar <adailsonacj@live.com>
2019-12-07 11:22:59 -0300

    feat(Header): alterar styles

---
+++ src/components/Header/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  background: #121212;
+  height: 50px;
+  flex-direction: row;
+  align-items: center;
+  padding-left: 20px;
+  padding-right: 20px;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  color: #03dac5;
+  font-size: 17px;
+  font-weight: bold;
+`;
