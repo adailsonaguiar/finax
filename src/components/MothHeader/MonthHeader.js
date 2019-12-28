b0ae1d9c24533e1fa749f0c0a533623db0dbd540
Adailson Aguiar <adailsonacj@live.com>
2019-12-27 22:53:29 -0300

    alternar meses ao tocar nos botões

--- src/components/MothHeader/MonthHeader.js
+++ src/components/MothHeader/MonthHeader.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {Container, Month} from './styles';
 import months from '../../utils/months';
+import {TouchableOpacity} from 'react-native';
 
 export default function MothHeader() {
   const [month, setMonth] = useState('');
@@ -18,11 +19,32 @@ export default function MothHeader() {
     setYear(date.getFullYear());
   };
 
+  const nextMonth = () => {
+    if (month === 11) {
+      setMonth(0);
+      setYear(year + 1);
+    } else {
+      setMonth(month + 1);
+    }
+  };
+  const previousMonth = () => {
+    if (month === 0) {
+      setMonth(11);
+      setYear(year - 1);
+    } else {
+      setMonth(month - 1);
+    }
+  };
+
   return (
     <Container>
-      <Icon name="keyboard-arrow-left" color="#fff" size={30} />
+      <TouchableOpacity onPress={previousMonth}>
+        <Icon name="keyboard-arrow-left" color="#fff" size={30} />
+      </TouchableOpacity>
       <Month>{`${months[month]}/${year}`}</Month>
-      <Icon name="keyboard-arrow-right" color="#fff" size={30} />
+      <TouchableOpacity onPress={nextMonth}>
+        <Icon name="keyboard-arrow-right" color="#fff" size={30} />
+      </TouchableOpacity>
     </Container>
   );
 }
